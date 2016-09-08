f0b4382a22859815f965f1f5d017b0e5627f2214
QueryQueen <Joedid@gmail.com>
2016-09-08 21:52:20 +0200

    testing travis

---
+++ test.sh
@@ -0,0 +1,21 @@
+cd $(dirname $0)
+
+mvn clean package
+
+# if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; 
+#   then 
+#     curl https://raw.githubusercontent.com/timkay/aws/master/aws -o aws
+#     chmod u+x aws
+#     ./aws put --progress "x-amz-acl: public-read" springio-guides/gs-rest-service-0.1.0.jar target/gs-rest-service-0.1.0.jar
+# fi
+
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+
+rm -rf build
+exit
+
